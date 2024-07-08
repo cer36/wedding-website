18ce5ed7a3de5ba421806ca03eb70b0553a04d01
Chris Evans <chris@ceva24.dev>
2024-07-08 21:35:54 +0100

    add sample section with image

---
+++ src/components/section.tsx
@@ -0,0 +1,39 @@
+import * as React from "react"
+import { FC, PropsWithChildren } from "react";
+
+interface SectionProps {
+    style: "standard" | "alt";
+    heading: string;
+    image?: React.ReactElement;
+}
+
+const Section: FC<PropsWithChildren<SectionProps>> = ({
+    style,
+    heading,
+    image,
+    children,
+}: PropsWithChildren<SectionProps>) => {
+    return (
+        <section
+            className={`py-24 2xl:px-64 lg:px-48 md:px-24 px-12 ${
+                style === "alt" ? " bg-emerald-700 text-slate-100 text-right" : ""
+            }`}
+        >
+            <div className="2xl:flex 2xl:gap-36">
+                <div className={`self-center ${style === "alt" ? " pb-8 xl:pb-0 xl:order-last" : ""}`}>
+                    <h2 className="font-dk-crayon-crumble mb-12 tracking-tight leading-none text-5xl lg:text-6xl">
+                        {heading}
+                    </h2>
+                    {children}
+                </div>
+                {image && (
+                    <div className="pt-8 shrink-0 flex justify-center self-center">
+                        <div className="md:shrink-0">{image}</div>
+                    </div>
+                )}
+            </div>
+        </section>
+    );
+};
+
+export default Section;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,22 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+import Section from "../components/section"
 
 const title = "Claire & Chris' Wedding Party"
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <>
-  <header className="flex justify-center content-center text-6xl py-24 bg-slate-50 font-playfair"> <h1>{title}</h1></header>
-    <main>
-
-
-    
-    </main>
-     </>
+    <div className="flex justify-center bg-slate-100 text-slate-800 text-xl font-quicksand">
+            <div className="w-[1920px]">
+      <header className="flex justify-center content-center text-6xl py-24 bg-slate-50 font-playfair"> <h1>{title}</h1></header>
+      
+      <main>
+        <Section style="standard" heading="Hello" image={<StaticImage src="../images/otters.jpeg" alt="Otters" />}>
+        <p>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test </p>
+        </Section>
+      </main>
+    </div></div>
   )
 }
 
