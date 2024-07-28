10bf5b55fcca1cde755af682186fd20737c126fe
Chris Evans <chris@ceva24.dev>
2024-07-28 18:40:26 +0100

    remove path prefixes

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -6,9 +6,8 @@ dotenv.config({ path: ".env" });
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `wedding-website`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.claireandchris.wedding`
   },
-  pathPrefix: "/wedding-website",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
